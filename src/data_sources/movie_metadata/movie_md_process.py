9f94cce959591522f700c26142d697028c894a57
AnsonLiu01 <ansonkliu@outlook.com>
2025-12-27 01:04:32 +0000

    updating tmdb class

--- src/data_sources/movie_metadata/movie_md_process.py
+++ src/data_sources/movie_metadata/movie_md_process.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 class MovieMetaData:
     """
-    Class for Movie metadata functions
+    Class for TMDB Movie metadata functions
     """
     def __init__(
         self,
@@ -25,41 +25,20 @@ class MovieMetaData:
         Function to load data
         """        
         self.df = pd.read_csv(os.path.join(self.root_dir, 'data', self.file_loc))
-        
-    def process_col_names(self) -> None: 
-        """
-        Function to add column names to raw data frame
-        """
-        col_names = ['wiki_movie_id', 'freebase_movie_id', 'name', 'release_date', 'revenue', 'runtime', 'languages', 'countries', 'genres']
-        relevant_cols = ['name', 'release_date', 'revenue', 'runtime', 'genres']
-        
-        self.df.columns = col_names
-        
-        self.df = self.df[relevant_cols]
-        
+                
     def clean_data(self) -> None:
         """
         Function to clean and process the data
         """
-        self.df["release_year"] = (
-            self.df["release_date"]
-            .astype(str)
-            .str.extract(r"(\d{4})")
-            .fillna('9999')
-            .astype(int)
-        )
-
-        self.df["genres_list"] = self.df["genres"].fillna("").astype(str).str.findall(r':\s*"([^"]+)"')
-
-        mask_dicts = self.df["genres"].apply(lambda x: isinstance(x, dict))
-        if mask_dicts.any():
-            self.df.loc[mask_dicts, "genres_list"] = self.df.loc[mask_dicts, "genres"].apply(lambda d: list(d.values()))
-
-        self.df["genres_list"] = self.df["genres_list"].apply(
-            lambda lst: [g.strip().lower() for g in dict.fromkeys(lst)] if isinstance(lst, list) else []
-        )
+        keep_cols = [
+            'title', 'release_date', 'revenue', 'budget', 'genres', 'production_companies', 'production_countries', 'keywords', 'overview'
+        ]
         
-        self.df = self.df[['name', 'release_year', 'genres_list', 'revenue']]
+        self.df = self.df[keep_cols]
+        
+        self.df['release'] = self.df['release_date'].str.split('-')[0].astype(int)
+        
+        self.df = self.df.drop(columns=['release_date'])
         
     def preprocess_runner(self) -> None:
         """
@@ -67,8 +46,6 @@ class MovieMetaData:
         """
         self.load_data()
         
-        self.process_col_names()
-
         self.clean_data()
     
     def runner(self) -> None:
